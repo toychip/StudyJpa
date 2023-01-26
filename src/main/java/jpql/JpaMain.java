948acbb6c38dc07f9cd791428aa0381a07491286
toychip <dlawnsgud427@naver.com>
2023-01-26 15:26:27 +0900

    01261530_페이징

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -15,57 +15,45 @@ public class JpaMain {
         //code
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+            System.out.println("\n************************ 객체 생성 ************************\n");
+
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i);
+//                System.out.println("\n************************ em.persist ************************\n");
+                em.persist(member);
+            }
+
+            System.out.println("\n************************ em.flush ************************\n");
+            em.flush();
+
+            System.out.println("\n************************ em.clear ************************");
+            em.clear();
+            System.out.println("\n************************ em.clear 종료 ************************");
+
+            System.out.println("\n************************ 페이징 ************************\n");
+            List<Member> pagingResult = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(10)
+                    .getResultList();
+            System.out.println("pagingResult.size = " + pagingResult.size());
+            for (Member member1 : pagingResult) {
+                System.out.println("member1 = " + member1);
+            }
+
 
 //            TypedQuery<Member> query = em.createQuery("select m from Member m where m.username = :username", Member.class);
 //            query.setParameter("username", "member1");
 //            Member singleResult = query.getSingleResult();
 //            System.out.println("singleResult = " + singleResult.getUsername());
             //      singleResult = member1
-//            위를 아래로 줄여씀
+//            // 위를 아래로 줄여씀
+//            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+//                    .setParameter("username", "member1")
+//                    .getSingleResult();
+//            System.out.println("singleResult = " + result.getUsername());
 
-
-            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
-            System.out.println("singleResult = " + result.getUsername());
-
-            em.flush();
-            em.clear();
-
-            // --------------------------------------------여러 값 조회--------------------------------------------
-            // Query 타입으로 조회
-            System.out.println("\n************************ Query 타입으로 조회 ************************\n");
-            List manyValue = em.createQuery("select m.username, m.age from Member m").getResultList();
-
-            Object o = manyValue.get(0);
-            Object[] insideValue = (Object[]) o;
-            System.out.println("username = " + insideValue[0]);
-            System.out.println("age = " + insideValue[1]);
-
-            // TypeQuery 방식, Object[]타입으로 조회
-            System.out.println("\n************************ TypeQuery 방식, Object[]타입으로 조회 ************************\n");
-            List<Object[]> TQWay = em.createQuery("select m.username, m.age from Member m").getResultList();
-
-            Object[] TQWayList = TQWay.get(0);
-            System.out.println("username = " + TQWayList[0]);
-            System.out.println("age = " + TQWayList[1]);
-
-//            ***************************new 명령어로 조회*************************** 제일 많이 사용되는 좋은 방법
-            // 단순 값을 DTO로 바로 조회
-            // ex) SELECT new jpabook.jpqlUserDTO(m.username, m.age) FROM Member m
-            // 패키지 명을 포함한 전체 클래스 명 입력
-            // 순서와 타입이 일치하는 생성자가 필요하다.
-
-            System.out.println("\n************************ new 명령어로 조회 ************************\n");
-            List<MemberDTO> newWay = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m",
-                            MemberDTO.class).getResultList();
-            MemberDTO memberDTO = newWay.get(0);
-            System.out.println("username = " + memberDTO.getUsername());
-            System.out.println("age = " + memberDTO.getAge());
 
 //            TypedQuery<Member> query = em.createQuery("select m From Member m ", Member.class);
 //            컬렉션 반환
@@ -95,6 +83,36 @@ public class JpaMain {
 //            Query query3 = em.createQuery("select m.username m.age From Member m ");
 
 
+//            // --------------------------------------------여러 값 조회--------------------------------------------
+//            // Query 타입으로 조회
+//            System.out.println("\n************************ Query 타입으로 조회 ************************\n");
+//            List manyValue = em.createQuery("select m.username, m.age from Member m").getResultList();
+//
+//            Object o = manyValue.get(0);
+//            Object[] insideValue = (Object[]) o;
+//            System.out.println("username = " + insideValue[0]);
+//            System.out.println("age = " + insideValue[1]);
+//
+//            // TypeQuery 방식, Object[]타입으로 조회
+//            System.out.println("\n************************ TypeQuery 방식, Object[]타입으로 조회 ************************\n");
+//            List<Object[]> TQWay = em.createQuery("select m.username, m.age from Member m").getResultList();
+//
+//            Object[] TQWayList = TQWay.get(0);
+//            System.out.println("username = " + TQWayList[0]);
+//            System.out.println("age = " + TQWayList[1]);
+//
+////            ***************************new 명령어로 조회*************************** 제일 많이 사용되는 좋은 방법
+//            // 단순 값을 DTO로 바로 조회
+//            // ex) SELECT new jpabook.jpqlUserDTO(m.username, m.age) FROM Member m
+//            // 패키지 명을 포함한 전체 클래스 명 입력
+//            // 순서와 타입이 일치하는 생성자가 필요하다.
+//
+//            System.out.println("\n************************ new 명령어로 조회 ************************\n");
+//            List<MemberDTO> newWay = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m",
+//                    MemberDTO.class).getResultList();
+//            MemberDTO memberDTO = newWay.get(0);
+//            System.out.println("username = " + memberDTO.getUsername());
+//            System.out.println("age = " + memberDTO.getAge());
             tx.commit();
         }catch (Exception e){
             tx.rollback();
--- src/main/java/jpql/Member.java
+++ src/main/java/jpql/Member.java
@@ -38,4 +38,14 @@ public class Member {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", age=" + age +
+//                ", team=" + team + ************** 양방향되면 무한루프에 빠지기 때문에 매핑한 것은 주석처리하기 **************
+                '}';
+    }
 }
