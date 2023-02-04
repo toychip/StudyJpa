911fead005672de68a3800b2900e4c5b928b8298
toychip <dlawnsgud427@naver.com>
2023-02-04 18:01:11 +0900

    02041800_InquiryMemberList

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -37,5 +38,12 @@ public class MemberController {
         member.setAddress(address);
         memberservice.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberservice.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
