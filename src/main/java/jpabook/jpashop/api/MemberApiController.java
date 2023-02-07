07602bc2999f1904ba3e385db4360a46db6805d2
toychip <dlawnsgud427@naver.com>
2023-02-07 15:15:00 +0900

    02071500_ApiFindMembers

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 //@Controller @ResponseBody 두개를 합쳐서 RestController로 적음
 // 데이터 자체를 json이나 api로 바로 보낼때 @ResponseBody를 사용함
@@ -17,6 +20,36 @@ import javax.validation.constraints.NotEmpty;
 public class MemberApiController {
 
     private final Memberservice memberservice;
+
+    @GetMapping("/api/v1/members")  // 응답 값으로 엔티티를 직접 외부에 노출
+    // 엔티티의 모든 값이 노출된다.
+    // 실제 현업에서 개발할때 이렇게 개발하면 안된다. 엔티티 수정시 스팩이 변경되며 오류가 날 수 있고 유지보수가 매우 어렵다.
+    public List<Member> membersV1(){
+        return memberservice.findMembers();
+    }
+    @GetMapping("/api/v2/members")
+    public Result memberV2(){
+        List<Member> findMembers = memberservice.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());  // ListMember 를 dto로 변환
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private int count;  // 이렇게 원하는 데이터를 직접 추가해서 할 수 있음 아래와 같이
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;        // 노출하고 싶은 것만 노출함 api스펙이 dto와 코드가 1대1이된다. 필요한것만 노출해서 좋다.
+    }
+
+
 
     //등록이므로 postMapping
     @PostMapping("/api/v1/members")     // 회원을 등록하는 api
@@ -27,6 +60,7 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
         // 여기서 문제는 어떠한 방식으로 회원가입할지 알 수 없으니 엔티티를 파라미터로 넣으면 안된다. dto를 사용해야하는 이유.
     }
+
 
     @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
@@ -48,8 +82,10 @@ public class MemberApiController {
         Member findMember = memberservice.findOne(id);
 
         return new UpdateMemberResponse(findMember.getId(), findMember.getName());
-        //response dto 반환
+        //response dto 반
     }
+
+
 
     @Data
     static class UpdateMemberRequest{
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain;
 
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,6 +25,7 @@ public class Member {
     @Embedded
     private Address address;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "member")     // order table에 있는 member에 의해서 mapping된것이다.
     private List<Order> orders = new ArrayList<>();
 
