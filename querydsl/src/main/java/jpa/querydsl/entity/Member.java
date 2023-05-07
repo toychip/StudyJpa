c4100aec24f841f9e27c0c094435ee5a2746f9cc
toychip <dlawnsgud427@naver.com>
2023-05-07 21:14:26 +0900

    05072100_QuerydslVsJPQL

---
+++ querydsl/src/main/java/jpa/querydsl/entity/Member.java
@@ -0,0 +1,45 @@
+package jpa.querydsl.entity;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})   // team 들어가면 무한루프 걸림
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this(username, 0);
+    }
+
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+
+        if(team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
--- querydsl/src/test/java/jpa/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/jpa/querydsl/QuerydslApplicationTests.java
@@ -7,6 +7,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -15,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTests {
 	@Autowired
 	EntityManager em;
