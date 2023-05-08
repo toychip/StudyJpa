dcfdccd2f5406553ef83f5e470dd0350f2dec61c
toychip <dlawnsgud427@naver.com>
2023-05-08 12:08:26 +0900

    05081200_SearchCondition

--- querydsl/src/test/java/jpa/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/jpa/querydsl/QuerydslBasicTest.java
@@ -5,6 +5,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpa.querydsl.entity.Member;
 import jpa.querydsl.entity.Team;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +19,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @Transactional
 public class QuerydslBasicTest {
+    @Test
+    @DisplayName("메뉴얼")
+    void test1(){
+        //given
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1"), // username = 'member1'
+                        member.username.ne("member1"), //username != 'member1'
+                        member.username.eq("member1").not(), // username != 'member1'
+                        member.username.isNotNull(), //이름이 is not null
+                        member.age.in(10, 20), // age in (10,20)
+                        member.age.notIn(10, 20), // age not in (10, 20)
+                        member.age.between(10,30),//between 10, 30
+                        member.age.goe(30), // age >= 30
+                        member.age.gt(30),// age > 30
+                        member.age.loe(30), // age <= 30
+                        member.age.lt(30), // age < 30
+                        member.username.like("member%"), //like 검색
+                        member.username.contains("member"), // like ‘%member%’ 검색
+                        member.username.startsWith("member") //like ‘member%’ 검색
+                )
+                .fetchOne();
+    }
+
 
     @Autowired
     EntityManager em;
@@ -68,4 +94,31 @@ public class QuerydslBasicTest {
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
+
+    @Test
+    void search() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1").and(member.age.eq(10)))
+                .fetchOne();
+        // 10살 & member1
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    void searchAndParam() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                )
+                .fetchOne();
+        // 10살 & member1
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    
 }
